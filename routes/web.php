a3f8ea5bb3ac8170df404954151679339d418b8b
Wasuremeno <wasuremono19@gmail.com>
2026-02-06 18:22:53 +0300

    feat: set up books routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
-
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('books.index');
 });
+
+Route::resource('books', BookController::class)->only(['index', 'create', 'store']);
